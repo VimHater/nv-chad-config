19185e1bc2eb22683adfad04c81d0219704474d3
VimHater <24520826@gm.uit.edu.vn>
2025-03-11 21:41:43 +0700

    update

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,8 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" , "clangd", "neomakelsp", "pyright" }
+local servers = { "html", "cssls" , "clangd", "pyright" }
+
 
 local nvlsp = require "nvchad.configs.lspconfig"
 
